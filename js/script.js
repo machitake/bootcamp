4e86fcd19b94e8fe0f784cca0da925deb9a1e6a8
machitake <glory.tm5@gmail.com>
2021-06-20 21:23:10 +0900

    ハンバーガーメニュー
    全画面表示時、スクロールさせない

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ $('.burger-btn').on('click',function(){ //.burger-btnをクリックすると
 
     $('.burger-btn').toggleClass('close'); //.burger-btnにcloseというclassを追加/削除する
     $('.nav-wrapper').fadeToggle(500); //.nav-wrapperがフェードイン/フェードアウトする
-
+    $('body').toggleClass('noscroll');//スクロールさせない
   });
